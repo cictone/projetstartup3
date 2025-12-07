39175388cf58b7191d2c8bf59494bff68f755dca
fgaetan <gaetan.flandrin@ynov.com>
2025-12-07 10:00:55 +0100

    startup

---
+++ jeux.js
@@ -0,0 +1,13 @@
+var img = document.createElement("img");
+img.src = "sdaah_image_rescaled.jpg";
+var block = document.getElementById("x");
+block.appendChild(img);
+
+
+
+var img = document.createElement("img");
+img.src = "scav_image_rescaled.png";
+var block = document.getElementById("x");
+block.appendChild(img);
+
+
