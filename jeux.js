8458215aae14bffcea7e71f454a3ef64cf59d700
fgaetan <gaetan.flandrin@ynov.com>
2025-12-11 14:49:38 +0100

    startup

--- jeux.js
+++ jeux.js
@@ -4,3 +4,22 @@ var block = document.getElementById("x");
 block.appendChild(img);
 
 
+var tImages = [ 'scav_image_rescaled.png', 'canard.jpg', 'chaton.jpg', 'ecureuil.jpg', 'yellow', 'lapin.jpg', 'suricate.jpg', 'tarentule.jpg', 'tigre.jpg' ];
+
+
+function demarage() {
+
+    var monImage = document.createElement('img');
+
+    monImage.src = tImages[1];
+
+    var maDiv = document.getElementById("bleu");
+
+    maDiv.innerHTML = '';
+
+    maDiv.appendChild(monImage);
+
+}
+
+
+window.addEventListener("load",demarrage);
--- scavpage.js
+++ scavpage.js
@@ -1,4 +1,4 @@
 var img = document.createElement("img");
 img.src = "scav_image_rescaled.png";
-var block = document.getElementById("scav");
+var block = document.getElementById("i");
 block.appendChild(img);
